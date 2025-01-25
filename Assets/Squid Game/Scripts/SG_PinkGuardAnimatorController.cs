eb8d6f8055b2cc1afe652889880c3744d9533048
SANTOSH-PC\Santhosh N <santosh.nallaa@gmail.com>
2025-01-25 11:00:56 +0530

    Basic IK and Gun Setup is done

--- Assets/Squid Game/Scripts/SG_PinkGuardAnimatorController.cs
+++ Assets/Squid Game/Scripts/SG_PinkGuardAnimatorController.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class SG_PinkGuardAnimatorController : MonoBehaviour
 {
+    [SerializeField] private bool enableTurnAnimation;
     [SerializeField] private Animator animator;
 
     private void Update()
     {
-        animator.SetFloat("Turn", SG_InputController.LookInput.x);
+        if (enableTurnAnimation)
+            animator.SetFloat("Turn", SG_InputController.LookInput.x);
     }
 }
